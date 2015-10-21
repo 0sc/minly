3075cdef72dbcf889bc45059d482f579eee86872
vagrant <vagrant@precise32.(none)>
2015-10-21 11:46:38 +0000

    Implement redirect of shortened url to original target. Create test stubs for redirect feature

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,10 +1,11 @@
 module UrlsHelper
   def create_shortened_url (url_id)
-      host_url + "/" + url_id.to_s(32).reverse
+      # host_url + "/" + url_id.to_s(32).reverse
+      url_id.to_s(32).reverse
   end
 
   def host_url
-    request.base_url
+    request.base_url + "/"
   end
 
   def sanitize_url(url)
