0db69571967f1259cf4bdc7c555c200763100085
vagrant <vagrant@precise32.(none)>
2015-10-25 21:38:12 +0000

    Add user association to Url table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023170859) do
+ActiveRecord::Schema.define(version: 20151025213610) do
 
   create_table "urls", force: :cascade do |t|
     t.string   "original"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20151023170859) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "views",      default: 0
+    t.integer  "user_id"
   end
+
+  add_index "urls", ["user_id"], name: "index_urls_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
