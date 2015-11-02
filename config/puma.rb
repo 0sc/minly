95fcd74363d4af62e5ee9d0c68c759cc548e7114
vagrant <vagrant@precise32.(none)>
2015-11-02 03:52:00 +0000

    Add more options to puma file

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,13 @@
-# config/puma.rb
+workers 4
+threads_count = 1
+threads threads_count, threads_count
+
+preload_app!
+
+rackup DefaultRackup
+port ENV['PORT'] || 3000
+environment ENV['RAILS_ENV'] || 'development'
+
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
--- db/migrate/20151101165633_drop_table_ahoy_events.rb
+++ db/migrate/20151101165633_drop_table_ahoy_events.rb
@@ -1,7 +1,6 @@
 class DropTableAhoyEvents < ActiveRecord::Migration
   def change
     drop_table :ahoy_events
-    drop_table :visits
 
     add_index :urls, :original
     add_index :urls, :shortened
